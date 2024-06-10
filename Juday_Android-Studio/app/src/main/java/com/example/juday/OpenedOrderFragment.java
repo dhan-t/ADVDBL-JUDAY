3e00251c271f30304eb011f00fd9a16c6a6b0e15
Dhan Tamparong <dhantcm@gmail.com>
2024-06-10 14:48:39 +0800

    all pages added.

--- Juday_Android-Studio/app/src/main/java/com/example/juday/OpenedOrderFragment.java
+++ Juday_Android-Studio/app/src/main/java/com/example/juday/OpenedOrderFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Home_Fragment#newInstance} factory method to
+ * Use the {@link OpenedOrderFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Home_Fragment extends Fragment {
+public class OpenedOrderFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Home_Fragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Home_Fragment() {
+    public OpenedOrderFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class Home_Fragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Home_Fragment.
+     * @return A new instance of fragment OpenedOrderFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Home_Fragment newInstance(String param1, String param2) {
-        Home_Fragment fragment = new Home_Fragment();
+    public static OpenedOrderFragment newInstance(String param1, String param2) {
+        OpenedOrderFragment fragment = new OpenedOrderFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,8 +59,6 @@ public class Home_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_, container, false);
+        return inflater.inflate(R.layout.fragment_opened_order, container, false);
     }
 }
-
-
